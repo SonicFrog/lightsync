ea7b907e3dec003c33700f2e6d13cebf13342d37
Ogier Bouvier <ogier.bouvier@epfl.ch>
2014-08-18 11:47:05 +0200

    Implemented control channel in ShareHandler and better message handling

--- sharehandler.go
+++ sharehandler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -27,7 +26,7 @@ func (sh *ShareHandler) Active() bool {
 
 func (sh *ShareHandler) HandleLocal() {
 	defer sh.Share.Watcher.Close()
-	for sh.Active() {
+	for {
 		select {
 		case evt := <- sh.Events() :
 			//Event
@@ -37,41 +36,85 @@ func (sh *ShareHandler) HandleLocal() {
 		case err := <- sh.Errors() :
 			//Error
 			fmt.Println("error: ", err)
+
+
+		case <- Control :
+			LogObj.Println("ShareHandler ", sh.Name, " stopping!")
 			break
 		}
+
 	}
 }
 
 func (sh *ShareHandler) Handle(msg Message) (err error) {
 	switch msg.(type) {
 	case FileRemoveMessage:
-	case DirectoryRemoveMessage:
+		msg, ok := msg.(FileRemoveMessage)
+
+		if !ok {
+			panic("Invalid message type while handling message in share")
+		}
+
 		sh.Remove(msg.Name())
 		break
 
 	case FileCreatedMessage:
-		sh.CreateFile(msg.Name())
+		msg, ok := msg.(FileCreatedMessage)
+
+		if !ok {
+			panic("Invalid message type while handling message in share")
+		}
+
+		sh.CreateFile(msg.FileName())
 		break
 
 	case FileHashMessage:
-	case FileUpdatedMessage:
-		sh.CheckHash(msg.Name(), msg.Payload())
-		break
+		msg, ok := msg.(FileHashMessage)
+		if !ok {
+			panic("Invalid message type while handling message!")
+		}
+
+		if msg.Share() != sh.Name {
+
+		}
+		sh.CheckHash(msg.FileName(), msg.Hash())
+
 
 	case ShareACKMessage:
+		msg, ok := msg.(ShareACKMessage)
+		if !ok {
+			panic("Fatal while handling message in share " + sh.Name)
+		}
+		if msg.Share() != sh.Name {
+			panic("Share "+sh.Name+" had to handle a message for "+msg.Share())
+		}
 		sh.AddClient(msg.Sender())
 		break
 
 	case ShareLeaveMessage:
+		msg, ok := msg.(ShareLeaveMessage)
+		if !ok {
+			panic("Wow!")
+		}
+
+		if msg.Share() != sh.Name {
+			panic("Share "+sh.Name+ " had to handle a message for "+msg.Share())
+		}
 		sh.RemoveClient(msg.Sender())
-		break
 
 	case DirectoryCreateMessage:
+		msg, ok := msg.(DirectoryCreateMessage)
+		if !ok {
+			panic("Invalid message type while handling message!")
+		}
+		if msg.Share() != sh.Name {
+			panic("ShareHandler received a message for another share!")
+		}
 		sh.CreateDir(msg.Name())
-		break
+
 
 	default:
-		err = errors.New("Invalid message type handed to ShareHandler!")
+		panic("Invalid message received in ShareHandler!!")
 	}
 
 	return
