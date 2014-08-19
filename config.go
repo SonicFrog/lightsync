be48e6f2c72a5fe64a79a0ccf15a174d46fdbe7c
Ogier Bouvier <ogier.bouvier@epfl.ch>
2014-08-19 12:04:38 +0200

    Support for JSON configuration file implemented

--- config.go
+++ config.go
@@ -1,15 +1,73 @@
 package main
 
 import (
-	"crypto/rsa"
-	"fmt"
+	"encoding/json"
+	"os"
 )
 
-type Configuration struct {
-	NodeName string
-	NodeKey  rsa.PrivateKey
+const (
+	DefaultConfigFile string = "/home/ars3nic/.config/lightsync.json"
+)
+
+type JSONConfiguration struct {
+	nodeName string
+	keyPath  string
+	certPath string
+	shares   []ShareConfig
+	clients  []ClientConfig
 }
 
-func (c *Configuration) init() {
-	fmt.Println("Initiated config")
+type ShareConfig struct {
+	name                string
+	path                string
+	authorizedClientsID []string
+}
+
+type ClientConfig struct {
+	name string
+	id   string
+}
+
+type ConfigurationObject interface {
+	NodeName() string
+	CertPath() string
+	KeyPath() string
+}
+
+func NewJSONConfiguration(filepath string) (c *JSONConfiguration, err error) {
+	LogObj.Println("Initializing config...")
+
+	jfile, err := os.Open(DefaultConfigFile)
+
+	if err != nil {
+		LogObj.Println("Unable to open config file:", err)
+		return
+	}
+
+	defer jfile.Close()
+
+	jdec := json.NewDecoder(jfile)
+
+	err = jdec.Decode(c)
+
+	if err != nil {
+		LogObj.Println("Unable to parse config file:", err)
+		return
+	}
+
+	LogObj.Println("Config read from", jfile.Name())
+
+	return
+}
+
+func (c *JSONConfiguration) CertPath() string {
+	return c.certPath
+}
+
+func (c *JSONConfiguration) KeyPath() string {
+	return c.keyPath
+}
+
+func (c *JSONConfiguration) NodeName() string {
+	return c.nodeName
 }
